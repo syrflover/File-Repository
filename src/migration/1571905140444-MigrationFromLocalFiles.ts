4f8daec4d43a0a45b56c4edfedc47ac241b08e33
Lee Tae Woo <syr@meu.works>
2019-10-24 19:33:41 +0900

    Update Migration

--- src/migration/1571905140444-MigrationFromLocalFiles.ts
+++ src/migration/1571905140444-MigrationFromLocalFiles.ts
@@ -8,21 +8,21 @@ export class MigrationFromLocalFiles1571905140444
     implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
         const directories = await fs.promises.readdir(
-            '/madome/file/Image/Library',
+            '/madome/file/image/library',
         );
 
         for (const directory of directories) {
             const files = await fs.promises.readdir(
-                `/madome/file/Image/Library/${directory}`,
+                `/madome/file/image/library/${directory}`,
             );
 
             for (const file of files) {
                 const stat = await fs.promises.stat(
-                    `/madome/file/Image/Library/${directory}/${file}`,
+                    `/madome/file/image/library/${directory}/${file}`,
                 );
 
                 const filepath = parseFilePathFromString(
-                    `Image/Library/${directory}/${file}`,
+                    `image/library/${directory}/${file}`,
                 );
                 const content_type = mimetypes.lookup(file);
                 const content_length = stat.size;
@@ -36,7 +36,7 @@ export class MigrationFromLocalFiles1571905140444
                 newFile.content_type = content_type;
                 newFile.content_length = content_length;
                 newFile.created_at = stat.birthtime;
-                newFile.created_at = stat.birthtime;
+                newFile.updated_at = stat.birthtime;
 
                 await queryRunner.manager.save(File, newFile);
             }
