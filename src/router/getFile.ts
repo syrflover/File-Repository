edd906da2a76cde91622a5a405648d800db23075
Lee TaeWoo <syr@meu.works>
2022-01-21 14:51:38 +0900

    serve files to if hasn't extension in filepath

--- src/router/getFile.ts
+++ src/router/getFile.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import { router } from '../router';
 import { parseFilePathFromContext } from '../lib/parseFilePath';
-import { getRepository } from 'typeorm';
+import { getRepository, Like } from 'typeorm';
 
 import File from '../entity/File';
 
@@ -9,6 +9,9 @@ import { env } from '../env';
 import { catcher } from './lib/catcher';
 import { v1 } from './lib/regURL';
 import { serve } from './lib/serve';
+import { logger } from '../logger';
+
+const extRegexp = /\.[a-z0-9]+$/i;
 
 router.get(v1, async (ctx) => {
     const filepath = parseFilePathFromContext(ctx);
@@ -16,7 +19,22 @@ router.get(v1, async (ctx) => {
     const fileRepo = getRepository(File);
 
     try {
-        const file = await fileRepo.findOne({ path: filepath });
+        let file: File | undefined;
+
+        // Request URL Path에 파일 확장자가 명시되어 있지 않은 경우에는
+        // Like으로 파일을 찾은 후,
+        // 파일 이름이 같은지 확인하고 같지 않으면 404, 같으면 정상적으로 보냄
+        if (!extRegexp.test(filepath)) {
+            file = await fileRepo.findOne({ path: Like(`${filepath}%`) });
+
+            const isSame = file?.path.replace(extRegexp, '').replace(filepath, '');
+
+            if (isSame?.length ?? 0 <= 0) {
+                file = undefined;
+            }
+        } else {
+            file = await fileRepo.findOne({ path: filepath });
+        }
 
         if (!file) {
             ctx.status = 404;
--- src/router/lib/regURL.ts
+++ src/router/lib/regURL.ts
@@ -1,1 +1,2 @@
-export const v1 = /\/v1\/.+\..+/i;
+// export const v1 = /\/v1\/.+\..+/i;
+export const v1 = /\/v1\/.+/i;
